fc87aa3dd72b05f734410ce28ce7005e78f9bfd4
Shravani2621 <Shravani.S@tatatechnologies.com>
2024-05-16 11:02:50 +0530

    Tosca Changes are Commited

---
+++ Execute.bat
@@ -0,0 +1,3 @@
+cd %COMMANDER_HOME%
+
+tcshell -workspace "D:\Tosca_Projects\Tosca_Devops\Tosca_Workspaces\Team Center Demo Use Cases\Team Center Demo Use Cases.tws" -login "" "" "C:\Tosca_Projects\ToscaGithub\script.tcs"
